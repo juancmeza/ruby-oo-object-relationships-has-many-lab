9ce4aeaabb60609665ca9655bd38a8daf4af7269
juan_carlos_meza <juancmezat@gmail.com>
2020-11-22 16:56:23 -0800

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -2,15 +2,8 @@ class Author
     
     attr_accessor :name, :post
 
-    @@all = []
-
     def initialize(name)
         @name = name
-        @@all << self
-    end
-
-    def self.all
-        @@all
     end
 
     def posts
